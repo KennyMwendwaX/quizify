1a6f5d5519a6cb54eda58ee3070c101222b60db6
KennyMwendwaX <kennymwendwadev@gmail.com>
2025-01-28 18:59:29 +0300

    feat: add database schema for quizzes, questions, and quiz responses

---
+++ src/database/schema.ts
@@ -0,0 +1,44 @@
+import { relations } from "drizzle-orm";
+import { boolean, integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const quizzes = pgTable("quizz", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  isTimeLimited: boolean("is_time_limited").notNull(),
+  timeLimit: integer("time_limit"),
+});
+
+export const quizzesRelations = relations(quizzes, ({ many }) => ({
+  questions: many(questions),
+  quizResponses: many(quizResponses),
+}));
+
+export const questions = pgTable("question", {
+  id: serial("id").primaryKey(),
+  quizId: integer("quiz_id")
+    .notNull()
+    .references(() => quizzes.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  text: text("text").notNull(),
+  choices: text("choices[]").array().notNull(),
+  correctAnswer: integer("correct_answer").notNull(),
+});
+
+export const questionsRelations = relations(questions, ({ one }) => ({
+  quiz: one(quizzes),
+}));
+
+export const quizResponses = pgTable("quiz_response", {
+  id: serial("id").primaryKey(),
+  quizId: integer("quiz_id")
+    .notNull()
+    .references(() => quizzes.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  userId: integer("user_id").notNull(),
+  answers: integer("answers[]").array().notNull(),
+  score: integer("score").notNull(),
+  timeTaken: integer("time_taken").notNull(),
+});
+
+export const quizResponsesRelations = relations(quizResponses, ({ one }) => ({
+  quiz: one(quizzes),
+}));
